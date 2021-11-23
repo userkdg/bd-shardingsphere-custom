ad1824b67e57a28b09e3d36d1da244a47bd66a83
Jarod Kong <1265598443@qq.com>
2021-11-23 14:56:08 +0800

    +自定义加密算法SM3/SM4

--- bd-shardingsphere-custom-encrypt-algo/src/test/java/cn/com/bluemoon/shardingsphere/encrypt/algorithm/SM4EncryptAlgorithmTest.java
+++ bd-shardingsphere-custom-encrypt-algo/src/test/java/cn/com/bluemoon/shardingsphere/encrypt/algorithm/SM4EncryptAlgorithmTest.java
@@ -27,7 +27,7 @@ public class SM4EncryptAlgorithmTest {
     @Before
     public void setUp() {
         Properties props = new Properties();
-        props.setProperty("sm4-key-value", "abc123efg");
+        props.setProperty("sm4-key-value", "abc123efgabc123efgabc123efgabc123efg");
         props.setProperty("sm4-iv-value", "bluemooniviviviv");
         encryptAlgorithm = ShardingSphereAlgorithmFactory
                 .createAlgorithm(new ShardingSphereAlgorithmConfiguration("SM4", props),
@@ -36,10 +36,10 @@ public class SM4EncryptAlgorithmTest {
 
     @Test
     public void assertEncrypt() {
-        assertThat(encryptAlgorithm.encrypt("18813975053"), is("Ztb20zufXcItdTCVqk+30w=="));
+        assertThat(encryptAlgorithm.encrypt("18813975053"), is("DyytBVpVZkwsoi1ce6nCmA=="));
         assertThat(encryptAlgorithm.encrypt(null), is(nullValue()));
 
-        assertThat(encryptAlgorithm.decrypt("Ztb20zufXcItdTCVqk+30w=="), is("18813975053"));
+        assertThat(encryptAlgorithm.decrypt("DyytBVpVZkwsoi1ce6nCmA=="), is("18813975053"));
         assertThat(encryptAlgorithm.decrypt(null), is(nullValue()));
     }
 
