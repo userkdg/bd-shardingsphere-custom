519d0592d40663bf8a297eca7d4eb34864ac874b
Jarod Kong <1265598443@qq.com>
2022-03-01 14:35:03 +0800

    feat:art:大数据-离线集成中发现impalaUDF无法基于SPI机制实例化算法，改为简单工厂模式实例化工厂

--- bd-kms-center/bd-kms-encrypt-aglo/src/main/java/org/apache/shardingsphere/infra/config/algorithm/ShardingSphereAlgorithmFactory.java
+++ bd-kms-center/bd-kms-encrypt-aglo/src/main/java/org/apache/shardingsphere/infra/config/algorithm/ShardingSphereAlgorithmFactory.java
@@ -19,21 +19,37 @@ package org.apache.shardingsphere.infra.config.algorithm;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.shardingsphere.encrypt.spi.EncryptAlgorithm;
 import org.apache.shardingsphere.infra.config.TypedSPIConfiguration;
 import org.apache.shardingsphere.spi.typed.TypedSPIRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ShardingSphere algorithm factory.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ShardingSphereAlgorithmFactory {
-    
+
+    /**
+     * 增加静态算法管理（由于impala中无法利用SPI机制进行实例化算法）
+     */
+    private static final Map<String, String> algorithms = new HashMap<String, String>(8) {{
+        put("MD5", "org.apache.shardingsphere.encrypt.algorithm.MD5EncryptAlgorithm");
+        put("AES", "org.apache.shardingsphere.encrypt.algorithm.AESEncryptAlgorithm");
+        put("RC4", "org.apache.shardingsphere.encrypt.algorithm.RC4EncryptAlgorithm");
+        put("SM3", "org.apache.shardingsphere.encrypt.algorithm.SM3EncryptAlgorithm");
+        put("SM4", "org.apache.shardingsphere.encrypt.algorithm.SM4EncryptAlgorithm");
+    }};
+
     /**
      * Create algorithm.
-     * 
+     *
      * @param typedSPIConfig typed SPI configuration
      * @param algorithmClass algorithm class
-     * @param <T> type of algorithm
+     * @param <T>            type of algorithm
      * @return algorithm
      */
     @SuppressWarnings("unchecked")
@@ -43,5 +59,17 @@ public final class ShardingSphereAlgorithmFactory {
             ((ShardingSphereAlgorithmPostProcessor) result).init();
         }
         return result;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @SneakyThrows
+    public static EncryptAlgorithm<Object, String> doCreateStaticAlgorithm(final TypedSPIConfiguration typedSPIConfig) {
+        if (algorithms.containsKey(typedSPIConfig.getType().toUpperCase())) {
+            EncryptAlgorithm<Object, String> result = (EncryptAlgorithm) Class.forName(algorithms.get(typedSPIConfig.getType().toUpperCase())).newInstance();
+            result.setProps(typedSPIConfig.getProps());
+            result.init();
+            return result;
+        }
+        throw new UnsupportedOperationException("不支持该加密类型");
     }
 }
